37a915c905d1aae70cd80ca326270559770351dc
barem <sarah.shaikh6780@gmail.com>
2026-05-21 12:36:06 -0700

    Add files via upload

---
+++ app/afive/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Image } from "@heroui/react";
+
+import { title } from "@/components/primitives";
+import ConfettiButton from "@/components/confettiButton";
+
+export default function afive() {
+  return (
+    <div>
+      {/* parent */}
+
+      <h1 className={title()}>A5: barem bridge :)</h1>
+
+      <Image
+        alt="barem bridge"
+        src="https://s3.amazonaws.com/comicgeeks/characters/avatars/73954.jpg?t=1742402922"
+        width={100}
+      />
+      <p>
+        1) barem&apos;s only trying to follow the vision of a woman he looked up to
+        dearly and wants to carry out her vision even if it meant having a few
+        casualities on the way.
+      </p>
+
+      <Image
+        alt="barem bridge"
+        src="https://pbs.twimg.com/media/GWGOCQoXgAA1OOg.png"
+        width={100}
+      />
+      <p>
+        2) he&apos;s the second and command of the chainsaw man church and shows his
+        appreciation for chainsaw man all the time!! he wants chainsaw man to
+        come back for love and peace all over the world!!!
+      </p>
+
+      <Image
+        alt="barem bridge"
+        src="https://ella.janitorai.com/bot-avatars/miFRSNRd9YNVrpJAva9J3.webp?width=1200"
+        width={100}
+      />
+      <p>
+        3) ...okay well he&apos;s not really all that good BUT he is a really good
+        villain and is well written for the most part lol... he&apos;s very good at
+        his job at torturing the main character and always moved the story
+        forward whenever he was involved.
+      </p>
+
+      <Image
+        alt="barem bridge"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAk_4dQo9uHE_24aSJVYyxmjLedxKS7bIKcQ&s"
+        width={100}
+      />
+      <ConfettiButton />
+
+      <Image
+        alt="barem bridge"
+        src="https://ella.janitorai.com/bot-avatars/MwuTG8G3fEt_GKxdNsf6N.webp?width=1200"
+        width={100}
+      />
+    </div>
+  );
+}
